e79a6fc63fe470049bbee4bde839d3800b0a0f21
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-08 16:22:06 +0000

    Or statements

---
+++ OR.py
@@ -0,0 +1,6 @@
+#So, today we will learn how to use "OR statements" or statements are to for example add more things so like this
+print("Is this bob") or print("Is the cath?")
+#Another example being.
+E=input("Name?")
+if E == "BOBBY" or "Jeff":
+    print("Welcome to the party")
